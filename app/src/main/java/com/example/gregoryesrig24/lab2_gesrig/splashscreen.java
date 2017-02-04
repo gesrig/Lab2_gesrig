3467fe264a1f03a416dcef2bd7932e041bcae53f
Gregory Esrig <gregoryesrig24@gregorys-air.dhcp.nd.edu>
2017-02-03 22:27:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gregoryesrig24/lab2_gesrig/splashscreen.java
@@ -0,0 +1,38 @@
+package com.example.gregoryesrig24.lab2_gesrig;
+
+/**
+ * Created by gregoryesrig24 on 2/3/17.
+ */
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
